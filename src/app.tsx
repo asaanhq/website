ecf4ea4c69396ac0168ee605faaf9a131800ffd7
Ankit Kumar <ankits.kvmdam@hotmail.com>
2022-05-07 19:28:03 +0100

    setup page

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,18 @@
-import type { Component } from 'solid-js';
+import type { Component } from 'solid-js'
 
+import { Container } from './components/container'
+
+import './app.css'
+import { Landing } from './sections/landing'
 
 const App: Component = () => {
-  return (
-    <div>
-      Hello
-    </div>
-  );
-};
+    return (
+        <Container.Outer>
+            <Container.Inner>
+                <Landing />
+            </Container.Inner>
+        </Container.Outer>
+    )
+}
 
-export default App;
+export default App
---
+++ src/sections/landing/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from '../../components/container'
+import { Header } from '../../components/header'
+
+export const Landing = () => {
+    return (
+        <Container.Section>
+            <Header />
+        </Container.Section>
+    )
+}
