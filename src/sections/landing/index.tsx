58cb757e46fda58cacaf9e55eb2e1fcda86286c2
Ankit Kumar <ankits.kvmdam@hotmail.com>
2022-05-07 20:02:15 +0100

    add landing section

--- src/sections/landing/index.tsx
+++ src/sections/landing/index.tsx
@@ -1,10 +1,27 @@
 import { Container } from '../../components/container'
 import { Header } from '../../components/header'
 
+import HeroImg from '../../assets/png/hero-img.png'
+import styles from './landing.module.css'
+
 export const Landing = () => {
     return (
-        <Container.Section>
+        <Container.Section class={styles.hero}>
             <Header />
+            <div class={styles.heroBox}>
+                <div class={styles.heroMsgC}>
+                    <div class={styles.taglineC}>
+                        <div class={styles.tagline}>Business Made Easy.</div>
+                        <div class={styles.taglinePara}>
+                            We strive to be a client obsessed company rather
+                            than focusing on competition.
+                        </div>
+                    </div>
+                </div>
+                <div class={styles.heroImgC}>
+                    <img src={HeroImg} class={styles.heroImg} alt="Asaan's" />
+                </div>
+            </div>
         </Container.Section>
     )
 }
