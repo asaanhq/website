e2eba8c34753d1662c373835d621b352fdae6bdc
Ankit Kumar <ankits.kvmdam@hotmail.com>
2022-05-10 13:37:34 +0100

    add contants css

---
+++ src/constants/headings.ts
@@ -0,0 +1,20 @@
+export const SERVICES_SECTION = {
+    main: 'Services we provide',
+    sub: 'We have expertise in',
+}
+
+export const WHY_CHOOSE_US_SECTION = {
+    main: 'Why choose us?',
+    sub: 'Our Values',
+}
+
+export const CONTACT_SECTION = {
+    main: 'Get in touch',
+    sub: 'Just say, Hi!',
+}
+
+export const HERO = {
+    main: 'Business Made Easy.',
+    // eslint-disable-next-line max-len
+    sub: 'We strive to be a client obsessed company rather than focusing on competition.',
+}
---
+++ src/constants/ids.ts
@@ -0,0 +1,6 @@
+export const sectionIDs = {
+    landing: 'landing-section',
+    services: 'services-section',
+    whyChooseUs: 'why-choose-us-section',
+    contact: 'contact-section',
+}
